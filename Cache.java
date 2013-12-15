e649f56cd5fc7c9f1a0d3d74d67dd53748602142
Mohamed Aboul Azm <mohamedtamer92@gmail.com>
2013-12-15 23:01:01 +0200

    msg

--- Cache.java
+++ Cache.java
@@ -8,11 +8,13 @@ public class Cache {
 	int cycles_access_memory;
 	int write_hit_policy; //1=write_throught 2=write_back
 	int write_miss_policy; //1=write allocate 2=write around
-	ArrayList<ActualCache> myList = new ArrayList<ActualCache>();
+	int levels;
+	ArrayList<ActualCache> DCache = new ArrayList<ActualCache>();
+	ArrayList<ActualCache> ICache = new ArrayList<ActualCache>();
 
 	public Cache(int s, int l, int m, int cycles_access_data,
 			int cycles_access_memory, int write_hit_policy,
-			int write_miss_policy) {
+			int write_miss_policy, int levels) {
 		S = s;
 		L = l;
 		this.m = m;
@@ -20,8 +22,14 @@ public class Cache {
 		this.cycles_access_memory = cycles_access_memory;
 		this.write_hit_policy = write_hit_policy;
 		this.write_miss_policy = write_miss_policy;
-		for (int i = 0; i<m; i++) {
-			myList.add(new ActualCache(new Object[S/L][7]));
+		this.levels = levels;
+		int i;
+		for (i = 0; i<m; i++) {
+			DCache.add(new ActualCache(new Object[S/L][7]));
+		}
+		
+		for (i = 0; i<m; i++) {
+			ICache.add(new ActualCache(new Object[S/L][7]));
 		}
 	}
 	
