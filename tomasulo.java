e9c10e190eb1ca0bda90ef9c07c583164bbd3573
Omar Hossam <omar.csis101@gmail.com>
2013-12-18 20:51:35 +0200

    initiate

--- tomasulo.java
+++ tomasulo.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
 
-public class tomasulo {
-
+public class Tomasulo {
+	ArrayList<Character> rob = new ArrayList<Character>();
+	int robpointer;
+	ArrayList<String[]> rs = new ArrayList<String[]>();
+	ArrayList<Integer> state = new ArrayList<Integer>();
+	public Tomasulo(){
+		
+	}
+	
+	public void start(ArrayList<Register> regs, String[] ins){
+	
+	}
 }
