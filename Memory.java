a0f109c8e9aa743b60f3a080655a464573a6bce8
Mohamed Aboul Azm <mohamedtamer92@gmail.com>
2013-12-20 15:45:47 +0200

    added comments in cache and removed time in memory

--- Memory.java
+++ Memory.java
@@ -4,7 +4,7 @@ public class Memory {
         ArrayList<String> memory = new ArrayList<String>();
         ArrayList<Integer> index = new ArrayList<Integer>();
         
-        public String readData(String address, int time){
+        public String readData(String address){
         		if (index.contains(address)) {
                         int i = index.indexOf(address);
                         return memory.get(i);
